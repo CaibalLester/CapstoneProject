ba1b56b0797d972eefe2051cdc29e3a3a8d0c244
EscaleraJandel123 <134498020+EscaleraJandel123@users.noreply.github.com>
2023-11-22 23:54:22 +0800

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,10 +5,18 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+
 $routes->get('/AdDash', 'AdminController::AdDash');
 $routes->get('/ManageAgent', 'AdminController::ManageAgent');
 $routes->get('/ManageApplicant', 'AdminController::ManageApplicant');
 $routes->get('/AdProfile', 'AdminController::AdProfile');
 $routes->get('/AdSetting', 'AdminController::AdSetting');
 $routes->get('/AdHelp', 'AdminController::AdHelp');
+
+
+$routes->get('/', 'HomepageController::index');
+
+$routes->get('/ApplicantHome', 'ApplicantController::index');
+$routes->get('/Form1', 'ApplicantController::form1');
+$routes->post('/form1sv', 'ApplicantController::form1sv');
+
