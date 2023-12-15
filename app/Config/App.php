c283378046411c6b73c4947358ceda28e577517c
EscaleraJandel123 <134498020+EscaleraJandel123@users.noreply.github.com>
2023-12-15 23:10:08 +0800

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://allianzpnb.info/';
+    public string $baseURL = 'https://pnballianz.online/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
